c8b2809884298ceaf95d7a5197aa9d3ee110089c
ngwesse elvis <elvisdemoprojects@outlook.com>
2020-11-10 13:46:13 +0000

    changes

--- test/test.js
+++ test/test.js
@@ -4,16 +4,15 @@ let home = require('../Pages/HomePage');
 
 
 describe('Click On Login Button On HomePage',function() {
+    this.timeout(50000);
     
     it('First test case', async function(){
         await hooks.mochaHooks.beforeAll();
         await home.pageObjects.clickOnLoginButton();
         var bool = await hooks.mochaHooks.afterAll();
         assert.ok(bool)
-        console.log('First Testing complete');
     }),
 
     it('Second Test case', async function(){
-        console.log('Second Testing complete');
     })
 });
